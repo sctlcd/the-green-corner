ec917fcaa1fa12c23c7158ed1a3ade8422744e12
8 <lucinda.souchet@outlook.com>
2020-08-19 15:16:34 +0100

    connect Flask to the mongoDB and test connection => ok

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'the_green_corner'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello Plants. Coucou les plantes'
+@app.route('/get_plants')
+def get_plants():
+    return render_template("plants.html", plants = mongo.db.plants.find())
 
 
 if __name__ == '__main__':
