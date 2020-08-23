cafa493689bd0c984b12c233ba3520a736d24a94
8 <lucinda.souchet@outlook.com>
2020-08-24 00:17:23 +0100

    add init.js

---
+++ static/assets/js/init.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('.collapsible').collapsible();
+  $('select').material_select();
+
+  $('.modal').modal();
+});
