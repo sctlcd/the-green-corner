70431d9bc7670a8830e818eaff904261ea205a74
8 <lucinda.souchet@outlook.com>
2020-08-28 02:43:05 +0100

    update addplant.html, add required fields, add sections names

--- static/assets/js/init.js
+++ static/assets/js/init.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
   $('select').material_select();
 
   $('.modal').modal();
+
+  // for select "required" attribute
+  $("select[required]").css({
+    display: "inline",
+    height: 0,
+    padding: 0,
+    width: 0
+  });
 });
